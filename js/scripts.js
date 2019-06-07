081e853326acb279b51dd37226072705fbe9a387
dennisnyamweya <denisnyamweya813@gmail.com>
2019-06-07 14:44:17 +0300

    added scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+function myFunction() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value.indexOf("@");
+  var message = document.getElementById("message").value;
+
+  if(name.length<10){
+    alert("Enter your full name");
+  }
+  if (email == -1) {
+   alert("Not a valid e-mail!");
+ }
+ if(message<=20){
+   alert("Enter message");
+ }
+if(name.length>10 || email !==-1 && message>20){
+   alert("We have received your message. Thank you for reaching out to us.");
+ }
+}
+/*----------design toggle---------------*/
+  $(document).ready(function(){
+        $('.desi').show();
+        $('.des-hidden').click(function(){
+          $('.desi').hide();
+            $('.des-showing').toggle();
+});
+      $('.des-showing').click(function(){
+        $('.des-showing').hide();
+         $('.desi').toggle();
+});
+});
+/*----------development toggle---------------*/
+$(document).ready(function(){
+      $('.dev').show();
+      $('.dev-hidden').click(function(){
+        $('.dev').hide();
+          $('.dev-showing').toggle();
+});
+    $('.dev-showing').click(function(){
+      $('.dev-showing').hide();
+       $('.dev').toggle();
+});
+});
+/*----------product development toggle---------------*/
+$(document).ready(function(){
+      $('.prod').show();
+      $('.prod-hidden').click(function(){
+        $('.prod').hide();
+          $('.prod-showing').toggle();
+});
+    $('.prod-showing').click(function(){
+      $('.prod-showing').hide();
+       $('.prod').toggle();
+});
+});
